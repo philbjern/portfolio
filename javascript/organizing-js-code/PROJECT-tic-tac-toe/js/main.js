d188df73332a794d556e8fcb7e7b770807665da2
fbiernat <filipbiernat@gmail.com>
2021-11-26 14:18:44 +0100

    Implement AI turn and checking for winner

--- javascript/organizing-js-code/PROJECT-tic-tac-toe/js/main.js
+++ javascript/organizing-js-code/PROJECT-tic-tac-toe/js/main.js
@@ -1,5 +1,5 @@
 const GameBoard = (() => {
-  let board = ["O", "X", "", "", "", "", "", "", ""];
+  let board = ["", "", "", "", "", "", "", "", ""];
 
   const getBoard = () => board;
 
@@ -9,20 +9,66 @@ const GameBoard = (() => {
 
   const markField = (index, symbol) => {
     board[index] = symbol;
+    DisplayController.update();
   };
 
-  return { getBoard, reset, markField };
+  const checkForWinner = () => {
+    if (
+      // horizontal
+      (board[0] === "X" && board[1] === "X" && board[2] === "X") ||
+      (board[3] === "X" && board[4] === "X" && board[5] === "X") ||
+      (board[6] === "X" && board[7] === "X" && board[8] === "X") ||
+      // vertical
+      (board[0] === "X" && board[3] === "X" && board[6] === "X") ||
+      (board[1] === "X" && board[4] === "X" && board[7] === "X") ||
+      (board[2] === "X" && board[5] === "X" && board[8] === "X") ||
+      // diagonal
+      (board[0] === "X" && board[4] === "X" && board[8] === "X") ||
+      (board[6] === "X" && board[4] === "X" && board[2] === "X")
+    ) {
+      if (Game.getPlayer1().getSymbol() === "X") {
+        return Game.getPlayer1();
+      } else {
+        return Game.getPlayer2();
+      }
+    } else if (
+      // horizontal
+      (board[0] === "O" && board[1] === "O" && board[2] === "O") ||
+      (board[3] === "O" && board[4] === "O" && board[5] === "O") ||
+      (board[6] === "O" && board[7] === "O" && board[8] === "O") ||
+      // vertical
+      (board[0] === "O" && board[3] === "O" && board[6] === "O") ||
+      (board[1] === "O" && board[4] === "O" && board[7] === "O") ||
+      (board[2] === "O" && board[5] === "O" && board[8] === "O") ||
+      // diagonal
+      (board[0] === "O" && board[4] === "O" && board[8] === "O") ||
+      (board[6] === "O" && board[4] === "O" && board[2] === "O")
+    ) {
+      if (Game.getPlayer2().getSymbol() === "O") {
+        return Game.getPlayer2();
+      } else {
+        return Game.getPlayer1();
+      }
+    }
+    return false;
+  };
+
+  return {
+    getBoard,
+    reset,
+    markField,
+    checkForWinner,
+  };
 })();
 
 const DisplayController = (() => {
-  toggleDarkMode = () => {
+  const toggleDarkMode = () => {
     const body = document.querySelector("body");
     body.classList.toggle("dark-mode");
   };
 
   const update = () => {
     const gameFields = document.querySelectorAll(".gamefield");
-    console.log(GameBoard.getBoard());
     gameFields.forEach((field, index) => {
       field.classList.remove("player-X");
       field.classList.remove("player-O");
@@ -51,7 +97,7 @@ const DisplayController = (() => {
         const symbol = Game.getPlayerTurn().getSymbol();
         if (symbol === "X") {
           GameBoard.markField(index, symbol);
-          update();
+          Game.nextTurn();
         }
       });
     });
@@ -60,11 +106,11 @@ const DisplayController = (() => {
   const restartButton = () => {
     const restartBtn = document.querySelector(".restart-btn");
     restartBtn.addEventListener("click", (e) => {
-      Game.restart();
+      Game.start();
     });
   };
 
-  const toggleDarkModeEventListener = () => {
+  const addToggleDarkModeEventListener = () => {
     const darkModeToggleButton = document.querySelector(".dark-mode-toggle");
     darkModeToggleButton.addEventListener("click", (e) => {
       DisplayController.toggleDarkMode();
@@ -72,7 +118,19 @@ const DisplayController = (() => {
     });
   };
 
-  return { toggleDarkMode, toggleDarkModeEventListener, update, addFieldClickEventListener, restartButton };
+  const setMessage = (text) => {
+    const messageDiv = document.querySelector(".message");
+    messageDiv.textContent = text;
+  };
+
+  return {
+    toggleDarkMode,
+    addToggleDarkModeEventListener,
+    update,
+    addFieldClickEventListener,
+    restartButton,
+    setMessage,
+  };
 })();
 
 function Player(name, symbol) {
@@ -82,34 +140,96 @@ function Player(name, symbol) {
 }
 
 const Game = (() => {
+  let player1;
+  let player2;
   let playerTurn;
+
+  const getPlayer1 = () => player1;
+
+  const getPlayer2 = () => player2;
 
   const getPlayerTurn = () => playerTurn;
 
+  const setPlayerTurn = (player) => {
+    playerTurn = player;
+  };
+
   const init = () => {
-    DisplayController.toggleDarkModeEventListener();
+    DisplayController.addToggleDarkModeEventListener();
     DisplayController.restartButton();
+    DisplayController.addFieldClickEventListener();
+  };
+
+  const setPlayers = () => {
+    player1 = Player("Player 1", "X");
+    player2 = Player("Player 2", "O");
+    changePlayer(player1);
   };
 
   const start = () => {
-    console.log(GameBoard.board);
-    DisplayController.update();
-    DisplayController.addFieldClickEventListener();
-    const player1 = Player("Player 1", "X");
-    const player2 = Player("Player 2", "O");
-    playerTurn = player1;
-    console.log(getPlayerTurn().getSymbol());
-    // waitForPlayerTurn();
-    // checkForWinner();
-    // nextTurn();
-  };
-
-  const restart = () => {
     GameBoard.reset();
     DisplayController.update();
+    setPlayers();
   };
 
-  return { init, start, getPlayerTurn, restart };
+  const changePlayer = (player) => {
+    setPlayerTurn(player);
+    DisplayController.setMessage(`${playerTurn.getName()} turn`);
+  };
+
+  const nextTurn = () => {
+    let winner;
+    if (GameBoard.checkForWinner() === player1) {
+      // Game ends
+      // Player1 wins
+      winner = player1;
+      // Set to disable further marking of fields
+      setPlayerTurn(player2);
+      DisplayController.setMessage(`${winner.getName()} wins!`);
+    } else {
+      changePlayer(player2);
+      AITurn();
+      setTimeout(() => {
+        if (GameBoard.checkForWinner() === player2) {
+          // AI wins
+          winner = player2;
+          // Set to disable further marking of fields
+          setPlayerTurn(player2);
+          DisplayController.setMessage(`${winner.getName()} wins!`);
+        }
+      }, 501);
+    }
+  };
+
+  const AITurn = () => {
+    console.log("AI turn");
+    setTimeout(() => {
+      const board = GameBoard.getBoard();
+      let indexes = [];
+      for (let i = 0; i < board.length; i++) {
+        if (board[i] === "") {
+          indexes.push(i);
+        }
+      }
+      if (indexes.length > 0) {
+        let move = indexes[Math.floor(Math.random() * indexes.length)];
+        GameBoard.markField(move, getPlayerTurn().getSymbol());
+        changePlayer(player1);
+      } else {
+        DisplayController.setMessage(`No more moves, game over`);
+      }
+    }, 500);
+  };
+
+  return {
+    init,
+    start,
+    getPlayerTurn,
+    getPlayer1,
+    getPlayer2,
+    start,
+    nextTurn,
+  };
 })();
 
 Game.init();
