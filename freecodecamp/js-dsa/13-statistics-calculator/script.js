f683f784826883b1e89e8d1660a26d87d4e9fef8
philbjern <filipbiernat@gmail.com>
2025-02-18 11:34:26 +0100

    feat: initial statistics calculator code

---
+++ freecodecamp/js-dsa/13-statistics-calculator/script.js
@@ -0,0 +1,18 @@
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const testArr1 = [1, 2, 3, 4, 5];
+const testArr2 = [1, 2, 3, 4, 5, 6];
+
+const getMedian = (array) => {
+  const sorted = array.sort((a, b) => a - b);
+}
+
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+  
+  const mean = getMean(numbers);
+
+  document.querySelector("#mean").textContent = mean;
+}
