9314421c02f538a8e1503e3a01a7bb1467eb0907
philbjern <filipbiernat@gmail.com>
2025-04-30 03:18:30 +0200

    feat: app changes

--- odin-project/react/first-react-app/src/App.jsx
+++ odin-project/react/first-react-app/src/App.jsx
@@ -1,17 +1,30 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function ListItem(props) {
+  return <li key={props.animal}>{props.animal}</li>
+}
 
+function List(props) {
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return <ListItem key={animal} animal={animal} />
+      })}
+    </ul>
+  )
+}
+
+function App() {
+  const animals = ["Cow", "Lion", "Snake", "Lizard"];
   return (
     <>
-      <h1>Hello world!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1>Animals:</h1>
+      <ul>
+        {animals.map(animal => {
+          return <li key={animal}>{animal.toUpperCase()}</li>
+        })}
+      </ul>
     </>
   )
 }
