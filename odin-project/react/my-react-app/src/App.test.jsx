2326e24f61dfcfad90e9fe0683067fa3a0ae54ea
philbjern <filipbiernat@gmail.com>
2025-07-15 23:21:46 +0200

    feat: add mocking section

--- odin-project/react/my-react-app/src/App.test.jsx
+++ odin-project/react/my-react-app/src/App.test.jsx
@@ -1,8 +1,9 @@
-import { describe, it, expect } from 'vitest';
+import { vi, describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import App from './App';
 import userEvent from '@testing-library/user-event';
+import CustomButton from './components/CustomButton';
 
 // describe('App', () => {
 //   it('renders headline', () => {
@@ -42,5 +43,33 @@ describe("App component", () => {
 
     await user.click(button);
     expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+  })
+})
+
+describe("CustomButton", () => {
+  it("should render a button with the text 'Click me'", () => {
+    render(<CustomButton onClick={() => {}} />);
+
+    const button = screen.getByRole("button", { name: "Click me" });
+    expect(button).toBeInTheDocument();
+  })
+
+  it("should call the onClick function when clicked", async () => {
+    const onClick = vi.fn();
+    const user = userEvent.setup();
+
+    render(<CustomButton onClick={onClick} />);
+    
+    const button = screen.getByRole("button", { name: "Click me" });
+    await user.click(button);
+
+    expect(onClick).toHaveBeenCalled();
+  })
+
+  it("should not call onClick function when it isn't clicked", () => {
+    const onClick = vi.fn();
+    render(<CustomButton onClick={onClick} />);
+
+    expect(onClick).not.toHaveBeenCalled();
   })
 })
