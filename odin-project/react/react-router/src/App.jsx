46b71cfa96cbb98c654eada55554341d1ac10473
philbjern <filipbiernat@gmail.com>
2025-07-25 18:35:09 +0200

    feat: react router examples

---
+++ odin-project/react/react-router/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom';
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default App
---
+++ odin-project/react/react-router/src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import Profile from './components/Profile'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/profile',
+    element: <Profile />,
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
