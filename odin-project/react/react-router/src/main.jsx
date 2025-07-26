9a79fa9c370b91400e71443203f5d73a22a4afea
philbjern <filipbiernat@gmail.com>
2025-07-26 10:19:21 +0200

    feat: nested routes

--- odin-project/react/react-router/src/main.jsx
+++ odin-project/react/react-router/src/main.jsx
@@ -4,6 +4,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import Profile from './components/Profile'
+import Spinach from './components/Spinach.jsx'
+import Popeye from './components/Popeye.jsx'
+import DefaultProfile from './components/DefaultProfile.jsx'
 
 const router = createBrowserRouter([
   {
@@ -13,6 +16,11 @@ const router = createBrowserRouter([
   {
     path: '/profile',
     element: <Profile />,
+    children: [
+      { index: true, element: <DefaultProfile/> },
+      { path: 'spinach', element: <Spinach /> },
+      { path: 'popeye', element: <Popeye /> },
+    ]
   }
 ]);
 
