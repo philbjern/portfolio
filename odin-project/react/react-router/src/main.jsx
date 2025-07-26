4e80f1e4c0c569442d491110d3dc60e3bf0d1c5f
philbjern <filipbiernat@gmail.com>
2025-07-26 10:32:33 +0200

    refactor: move routes to separate file

--- odin-project/react/react-router/src/main.jsx
+++ odin-project/react/react-router/src/main.jsx
@@ -2,24 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
-import Profile from './components/Profile'
-import Spinach from './components/Spinach.jsx'
-import Popeye from './components/Popeye.jsx'
-import DefaultProfile from './components/DefaultProfile.jsx'
-import ErrorPage from './components/ErrorPage.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/profile/:name',
-    element: <Profile />,
-  }
-]);
+import routes from './routes.jsx'
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
