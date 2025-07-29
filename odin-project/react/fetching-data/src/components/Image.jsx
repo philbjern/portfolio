2b16f04b5619580bcff1fc40ced50f3cd29d03d4
philbjern <filipbiernat@gmail.com>
2025-07-29 20:58:03 +0200

    feat: handling errors

--- odin-project/react/fetching-data/src/components/Image.jsx
+++ odin-project/react/fetching-data/src/components/Image.jsx
@@ -2,16 +2,24 @@ import { useEffect, useState } from "react";
 
 const Image = () => {
   const [imageURL, setImageURL] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors"})
-    .then((response) => response.json())
+    fetch("https://jsonplaceholder.tpicode.com/photos", { mode: "cors"})
+    .then((response) => {
+      if (response.status >= 400) {
+        throw new Error("Server error");
+      }
+      return response.json()
+    })
     .then((data) => {
       console.log(data[0].url);
       setImageURL(data[0].url)
     })
-    .catch((error) => console.error(error));
-  }, []);
+    .catch((error) => setError(error))
+    }, []);
+
+  if (error) return <p>A network error was encountered</p>
 
   return (
     imageURL && (
